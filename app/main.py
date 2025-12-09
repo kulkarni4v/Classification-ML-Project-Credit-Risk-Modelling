4f31df4ce62c730da1344c50dd487c9d51b196d7
Vishal <kulkarni6v@gmail.com>
2025-12-09 19:44:28 +0530

    minor

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from prediction_helper import predict  # Ensure this is correctly linked to prediction_helper.py
 
 # Set the page configuration and title
-st.set_page_config(page_title="Lauki Finance: Credit Risk Modelling", page_icon="📊")
+st.set_page_config(page_title="Pal's Finance: Credit Risk Modelling", page_icon="📊")
 st.title("Lauki Finance: Credit Risk Modelling")
 
 # Create rows of three columns each
